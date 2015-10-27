946e9e7c9d0ba6d5d1e68dccf493f0182ac12044
Glenn Hurley <glenn.hurley@gmail.com>
2015-10-27 00:19:00 -0400

    Adding all db settings back to settings file.

--- stats_tracker/settings.py
+++ stats_tracker/settings.py
@@ -80,18 +80,18 @@ WSGI_APPLICATION = 'stats_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         # 'ENGINE': 'django.db.backends.sqlite3',
-#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'stats_tracker',
-#         'USER': 'stats_tracker',
-#         'PASSWORD': 'stats_tracker',
-#         'HOST': '',
-#         'PORT': '',
-#     }
-# }
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stats_tracker',
+        'USER': 'stats_tracker',
+        'PASSWORD': 'stats_tracker',
+        'HOST': '',
+        'PORT': '',
+    }
+}
 
 
 # Internationalization
