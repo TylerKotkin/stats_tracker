2c741dfec0a7ef674a90753c67d8fdce97fcc2b0
Tyler Kotkin <Tylerkotkin@gmail.com>
2015-12-16 17:25:34 -0500

    created index view

--- stats_app/views.py
+++ stats_app/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from rest_framework import viewsets, permissions, status
 from .models import Activity, Stat
 from .serializers import ActivitySerializer, StatSerializer, UserSerializer
